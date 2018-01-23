cbd4d8c435b69090499c9eacce5bdb997d519067
Klaus Birken <klaus.birken@gmail.com>
2018-01-23 10:34:44 +0100

    Extended JNI header generation script.

--- src/main/java/org.nanosite.warp.jni/generateHeader.sh
+++ src/main/java/org.nanosite.warp.jni/generateHeader.sh
@@ -2,9 +2,10 @@
 cd bin
 
 javah -jni org.nanosite.warp.jni.Warp
+javah -jni org.nanosite.warp.jni.WarpPool
 javah -jni org.nanosite.warp.jni.WarpFunctionBlock
 javah -jni org.nanosite.warp.jni.WarpBehavior
 javah -jni org.nanosite.warp.jni.WarpStep
 
-mkdir ../../org.nanosite.warp/src-gen
-cp *.h ../../org.nanosite.warp/src-gen
+mkdir ../../../cpp/org.nanosite.warp/src-gen
+cp *.h ../../../cpp/org.nanosite.warp/src-gen
